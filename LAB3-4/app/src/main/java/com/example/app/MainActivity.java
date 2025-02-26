372a15d6e1d987057f3b7716ac48e6606814bd18
BalalauBogdan <bogdan262003@gmail.com>
2025-02-26 12:01:05 +0200

    LAB3-4

---
+++ LAB3-4/app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.purpleButton);
+        TextView name = findViewById(R.id.nameInputText);
+        TextView viewName = findViewById(R.id.nameDisplayText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hello " + name.getText() + "!", Toast.LENGTH_LONG).show();
+                viewName.setText("Hi " + name.getText());
+            }
+        });
+    }
+}
